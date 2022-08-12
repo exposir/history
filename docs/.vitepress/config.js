5b4d258fe3cf1f6acc2f6fba7e92687a3a3d0a61
孟世博 <897205285@qq.com>
2022-08-12 16:43:02 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,6 +18,7 @@ export default {
         items: [
           { text: "内参治国的那些事", link: "/内参治国的那些事/index.md" },
           { text: "周末快乐", link: "/周末快乐/index.md" },
+          { text: "《东宫变》前传之春华传", link: "《东宫变》前传之春华传.md" },
         ],
       },
     ],
