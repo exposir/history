8d34bfd9c891010947656a261a6ff457eabad710
孟世博 <897205285@qq.com>
2022-08-12 16:56:55 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,7 +18,10 @@ export default {
         items: [
           { text: "内参治国的那些事", link: "/内参治国的那些事/index.md" },
           { text: "周末快乐", link: "/周末快乐/index.md" },
-          { text: "《东宫变》前传之春华传", link: "《东宫变》前传之春华传.md" },
+          {
+            text: "《东宫变》前传之春华传",
+            link: "《东宫变》前传之春华传/index.md",
+          },
         ],
       },
     ],
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let fs = require("fs");
 
-const url = "./docs/周末快乐/index.md";
-const splitWord = "唯⼀微博：群众岱岱";
+const url = "./docs/《东宫变》前传之春华传/index.md";
+const splitWord =
+  "%E3%80%8A%E4%B8%9C%E5%AE%AB%E5%8F%98%E3%80%8B%E5%89%8D%E4%BC%A0%E4%B9%8B%E6%98%A5%E5%8D%8E%E4%BC%A0%20c3c8c02cdca844d189695832eb10f540/";
 
 const init = () => {
   fs.readFile(url, function (err, data) {
