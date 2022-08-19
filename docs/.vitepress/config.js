c306158ff22d29314287d40046707be24e623fba
孟世博 <897205285@qq.com>
2022-08-19 15:53:42 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,8 @@ export default {
   themeConfig: {
     siteTitle: "史海泛舟",
     nav: [
-      { text: "Guide", link: "/guide" },
+      { text: "吃瓜", link: "/周末快乐/" },
+      { text: "人民日报", link: "/rmrb/" },
       {
         text: "Dropdown Menu",
         items: [
@@ -12,22 +13,41 @@ export default {
         ],
       },
     ],
-    sidebar: [
-      {
-        text: "",
-        items: [
-          { text: "内参治国的那些事", link: "/内参治国的那些事/index.md" },
-          { text: "周末快乐", link: "/周末快乐/index.md" },
-          {
-            text: "《东宫变》前传之春华传",
-            link: "《东宫变》前传之春华传/index.md",
-          },
-          {
-            text: "kkndme",
-            link: "kkndme.md",
-          },
-        ],
-      },
-    ],
+    sidebar: {
+      "/周末快乐/": [
+        {
+          text: "Guide",
+          items: [
+            { text: "内参治国的那些事", link: "/内参治国的那些事/index.md" },
+            { text: "周末快乐", link: "/周末快乐/index.md" },
+            {
+              text: "《东宫变》前传之春华传",
+              link: "《东宫变》前传之春华传/index.md",
+            },
+            {
+              text: "kkndme",
+              link: "kkndme.md",
+            },
+          ],
+        },
+      ],
+      "/rmrb/": [
+        {
+          text: "Guide",
+          items: [
+            { text: "内参治国的那些事", link: "/内参治国的那些事/index.md" },
+            { text: "周末快乐", link: "/周末快乐/index.md" },
+            {
+              text: "《东宫变》前传之春华传",
+              link: "《东宫变》前传之春华传/index.md",
+            },
+            {
+              text: "kkndme",
+              link: "kkndme.md",
+            },
+          ],
+        },
+      ],
+    },
   },
 };
